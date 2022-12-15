90ef2d0ab1a3a222526622c2073104c1e8d955ba
Nicholas Siciliano <nsicilia@oswego.edu>
2022-12-15 13:57:19 -0500

    Initial Commit

---
+++ BlkBrdVoiceRecorder/BlkBrdVoiceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlkBrdVoiceRecorderApp.swift
+//  BlkBrdVoiceRecorder
+//
+//  Created by Nicholas Siciliano-Salazar  on 12/15/22.
+//
+
+import SwiftUI
+
+@main
+struct BlkBrdVoiceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
